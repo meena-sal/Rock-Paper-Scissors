4c6e113f0d0607d38f81830ea991d330bf3ca857
Meena Salib <meenasalib2004@gmail.com>
2023-09-12 17:42:56 -0700

    Add function that determines the winner of a round

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function getComputerChoice(){
     let random = Math.floor(Math.random() * 3);
     if (random == 0){
@@ -8,5 +7,25 @@ function getComputerChoice(){
         return "paper";
     } else {
         return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win! Rock beats scissors";
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        return "You lose! Paper beats rock";
+    } else if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You win! Paper beats rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors"){
+        return "You lose! Scissors beats paper";
+    } else if (playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win! Scissors beats paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock"){
+        return "You lose! Rock beats scissors";
+    } else {
+        return "Draw!";
     }
 }
