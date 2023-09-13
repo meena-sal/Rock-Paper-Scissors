7881ec5d3bdc878963a6227c62271623fc263bc9
Meena Salib <meenasalib2004@gmail.com>
2023-09-12 21:43:55 -0700

    Game fully ready to play by calling new game function

--- script.js
+++ script.js
@@ -26,6 +26,32 @@ function playRound(playerSelection, computerSelection){
     } else if (playerSelection == "scissors" && computerSelection == "rock"){
         return "You lose! Rock beats scissors";
     } else {
-        return "Draw!";
+        return "Draw! ";
+    }
+}
+
+function game(){
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++){
+        let playerInput = prompt("Rock, paper, or scissors?");
+        let outcome = playRound(playerInput, getComputerChoice());
+        outcome = outcome.slice(0, 5);
+        if (outcome == "You w"){
+            playerWins++;
+            console.log("You win the round!");
+        } else if (outcome == "You l"){
+            computerWins++;
+            console.log("You lose the round!");
+        } else {
+            console.log("It's a draw!");
+        }
+    }
+    if (playerWins > computerWins){
+        console.log("You win the whole game!");
+    } else if (playerWins < computerWins){
+        console.log("You lose the whole game!")
+    } else {
+        console.log("The game is a draw!");
     }
 }
