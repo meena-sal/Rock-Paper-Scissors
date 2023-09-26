5dd2402c3120d464b8894fb8187c6566c44d95fe
Meena Salib <meenasalib2004@gmail.com>
2023-09-26 00:49:10 -0700

    Add functional buttons and round status display

--- script.js
+++ script.js
@@ -30,23 +30,93 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+
 function game(){
     let playerWins = 0;
     let computerWins = 0;
-    for (let i = 0; i < 5; i++){
-        let playerInput = prompt("Rock, paper, or scissors?");
-        let outcome = playRound(playerInput, getComputerChoice());
+    // for (let i = 0; i < 5; i++){
+    //     let playerInput = prompt("Rock, paper, or scissors?");
+        // let outcome = playRound(playerInput, getComputerChoice());
+        // outcome = outcome.slice(0, 5);
+        // if (outcome == "You w"){
+        //     playerWins++;
+        //     console.log("You win the round!");
+        // } else if (outcome == "You l"){
+        //     computerWins++;
+        //     console.log("You lose the round!");
+        // } else {
+        //     console.log("It's a draw!");
+        // }
+    // }
+    const btn1 = document.createElement("button");
+    const btn2 = document.createElement("button");
+    const btn3 = document.createElement("button");
+    const container1 = document.createElement("div");
+    btn1.setAttribute('id', 'rock');
+    btn2.setAttribute('id', 'paper');
+    btn3.setAttribute('id', 'scissors');
+    btn1.textContent = "rock";
+    btn2.textContent = "paper";
+    btn3.textContent = "scissors";
+    container1.appendChild(btn1);
+    container1.appendChild(btn2);
+    container1.appendChild(btn3);
+    container1.style.display = 'flex';
+    container1.style.justifyContent = 'space-evenly';
+    container1.style.backgroundColor = "red";
+    document.body.appendChild(container1);
+    const container2 = document.createElement("div");
+    let roundResult = "pending";
+    document.body.appendChild(container2);
+    container2.textContent = roundResult;
+
+
+    btn1.addEventListener("click", () => {let outcome = playRound("rock", getComputerChoice());
         outcome = outcome.slice(0, 5);
         if (outcome == "You w"){
             playerWins++;
-            console.log("You win the round!");
+            roundResult = "You win the round!"
+            container2.textContent = roundResult;
         } else if (outcome == "You l"){
             computerWins++;
-            console.log("You lose the round!");
+            roundResult = "You lose the round!"
+            container2.textContent = roundResult;
         } else {
-            console.log("It's a draw!");
-        }
-    }
+            roundResult = "It's a draw!";
+            container2.textContent = roundResult;
+        }});
+
+    btn2.addEventListener("click", () => {let outcome = playRound("paper", getComputerChoice());
+        outcome = outcome.slice(0, 5);
+        if (outcome == "You w"){
+            playerWins++;
+            roundResult = "You win the round!"
+            container2.textContent = roundResult;
+        } else if (outcome == "You l"){
+            computerWins++;
+            roundResult = "You lose the round!"
+            container2.textContent = roundResult;
+        } else {
+            roundResult = "It's a draw!";
+            container2.textContent = roundResult;
+        }});
+
+    btn3.addEventListener("click", () => {let outcome = playRound("scissors", getComputerChoice());
+        outcome = outcome.slice(0, 5);
+        if (outcome == "You w"){
+            playerWins++;
+            roundResult = "You win the round!"
+            container2.textContent = roundResult;
+        } else if (outcome == "You l"){
+            computerWins++;
+            roundResult = "You lose the round!"
+            container2.textContent = roundResult;
+        } else {
+            roundResult = "It's a draw!";
+            container2.textContent = roundResult;
+        }});
+
+        
     if (playerWins > computerWins){
         console.log("You win the whole game!");
     } else if (playerWins < computerWins){
@@ -55,3 +125,5 @@ function game(){
         console.log("The game is a draw!");
     }
 }
+
+// Add a running score and text that gives outcome of round
